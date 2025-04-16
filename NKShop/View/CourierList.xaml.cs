e92c46d7ce17bf011442707d915a42473c54d802
glashenka20 <glafiratimchenko@gmail.com>
2025-04-16 14:31:29 +1000

    Изменение 2

---
+++ NKShop/View/CourierList.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace NKShop.View
+{
+    /// <summary>
+    /// Логика взаимодействия для CourierList.xaml
+    /// </summary>
+    public partial class CourierList : Window
+    {
+        public CourierList()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
